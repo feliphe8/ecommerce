020b408377f3905bf4d3eb8a60c339919d627f02
Feliphe Simões Carvalho <fsc_nfsu@hotmail.com>
2018-04-27 16:32:00 -0300

    Site produtos

--- site.php
+++ site.php
@@ -1,16 +1,22 @@
 <?php
 use \Hcode\Page;
+use \Hcode\Model\Product;
 
 
 $app->get('/', function() {
 
+  $products = Product::listAll();
 	//$sql = new Hcode\DB\Sql();
   //$results = $sql->select("SELECT * FROM tb_users");
   //echo json_encode($results);
 
   $page = new Page();
-  $page->setTpl("index");
+  $page->setTpl("index", [
+    'products'=>Product::checkList($products)
+  ]);
 
 });
+
+
 
 ?>
