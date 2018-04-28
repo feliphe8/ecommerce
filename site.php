5a775b1b01ab1124044e5ae1482dc1689697b363
Feliphe Simões Carvalho <fsc_nfsu@hotmail.com>
2018-04-28 20:33:36 -0300

    Adicionar ao carrinho

--- site.php
+++ site.php
@@ -53,7 +53,42 @@ $app->get("/products/:desurl", function($desurl){
 $app->get("/cart", function(){
   $cart = Cart::getFromSession();
   $page = new Page();
-  $page->setTpl("cart");
+  $page->setTpl("cart", [
+    'cart'=>$cart->getValues(),
+    'product'=>$cart->getProducts()
+  ]);
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+  $product = new Product();
+  $product->get((int)$idproduct);
+  $cart = Cart::getFromSession();
+  $qt = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+  for ($i=0; $i < $qtd ; $i++) {
+    $cart->addProduct($product);
+  }
+  header("Location: /cart");
+  exit;
+});
+
+//DELETA QUANTIDADE EX: UM PRODUTO COM 4 DE QUANTIDADE
+$app->get("/cart/:idproduct/minus", function($idproduct){
+  $product = new Product();
+  $product->get((int)$idproduct);
+  $cart = Cart::getFromSession();
+  $cart->removeProduct($product);
+  header("Location: /cart");
+  exit;
+});
+
+//REMOVE O PRODUTO INDEPENDENTE DA QUANTIDADE
+$app->get("/cart/:idproduct/remove", function($idproduct){
+  $product = new Product();
+  $product->get((int)$idproduct);
+  $cart = Cart::getFromSession();
+  $cart->removeProduct($product, true);
+  header("Location: /cart");
+  exit;
 });
 
 
