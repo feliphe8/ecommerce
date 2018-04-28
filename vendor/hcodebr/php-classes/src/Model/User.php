81f16972e9248dfb4d49a7175e55d9649d044cb1
Feliphe Simões Carvalho <fsc_nfsu@hotmail.com>
2018-04-28 18:30:42 -0300

    Carrinho de compras

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -7,6 +7,38 @@
     const SESSION = "User";
     const SECRET = "HcodePhp7_Secret";
 
+    public static function getFromSession()
+    {
+      $user = new User();
+
+      if(isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0)
+      {
+        $user->setData($_SESSION[User::SESSION]);
+      }
+
+      return $user;
+    }
+
+    public static function checkLogin($inadmin = true)
+    {
+      if(
+        !isset($_SESSION[User::SESSION]) ||
+         !$_SESSION[User::SESSION] ||
+          !(int)$_SESSION[User::SESSION]["iduser"] > 0
+        ){
+          //Não está logado
+          return false;
+        } else {
+          if($inadmin === true && (bool)$_SESSION[User::SESSION]['inadmin'] === true){
+            return true;
+          } else if ($inadmin === false) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+    }
+
     public static function login($login, $password){
       $sql = new Sql();
       $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(":LOGIN"=>$login));
@@ -28,11 +60,7 @@
     }
 
     public static function verifyLogin($inadmin = true){
-      if(!isset($_SESSION[User::SESSION]) ||
-       !$_SESSION[User::SESSION] ||
-        !(int)$_SESSION[User::SESSION]["iduser"] > 0 ||
-        (bool)$_SESSION[User::SESSION]["inadmin"] !== $inadmin){
-
+      if(User::checkLogin($inadmin)){
           header("Location: /admin/login");
           exit;
       }
